8b63c0758a6732074f17de07e5209fa9dcfd89f0
Amra <amusic5@etf.unsa.ba>
2020-09-26 15:30:56 +0200

    Footer done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,34 +1,36 @@
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { AiFillFacebook, AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 
 function footer() {
   return (
-    <div className="footer">
-      <Container>
-        <Row>
-          <Col>Amra Musić</Col>
-        </Row>
-        <Row>
-         <Col>Address
-         <Col>Rakovica, Česma 22</Col>
-         <Col>Sarajevo, BiH</Col>
-         </Col>
-          <Col>Contact
-          <Col>+387 61/079-824</Col>
-          <Col>amusic5@etf.unsa.ba</Col>
-          </Col>
-          <Col>Social
-          <Col>https://github.com/amra-music</Col>
-          <Col>https://www.linkedin.com/in/amra-music/</Col>
-          <Col>https://www.facebook.com/amra.music.543/</Col>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
+    <div className="footer-container">
+      <div className='footer-content'>
+        <div className='footer-content-title'>GET IN TOUCH</div>
+        <div className='contact-container'>
+          <div>Call me at +387 61-079-824</div>
+          <a class="white-nav-link" target="_blank" rel="noopener noreferrer" href="mailto:amusic5@etf.unsa.ba">amusic5@etf.unsa.ba</a>
+        </div>
+      </div>
+      <div className='footer-content'>
+        <div className='footer-content-title'>SOCIAL</div>
+        <div className='social-container'>
+          <a className="social-link" rel="noopener noreferrer" href="https://github.com/amra-music" target="_blank">
+            <AiFillGithub />
+          </a>
+          <a className="social-link" rel="noopener noreferrer" href="https://www.linkedin.com/in/amra-music" target="_blank">
+            < AiFillLinkedin />
+          </a>
+          <a className="social-link" rel="noopener noreferrer" href="https://www.facebook.com/amra.music.543" target="_blank">
+            <AiFillFacebook />
+          </a>
+        </div>
+      </div>
+      <div class="break"></div>
+      <div className='footer-content' style={{ marginLeft: "auto" }}>
+        <div className="footer-copyright">
           &copy;{new Date().getFullYear()} <a href="https://github.com/amra-music">Amra Musić</a>. All rights reserved.
-          </Col>
-        </Row>
-      </Container>
+      </div>
+      </div>
     </div>
   );
 }
