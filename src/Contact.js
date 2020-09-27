53420c2f776db53200d17b1f143067524512eedd
Amra <amusic5@etf.unsa.ba>
2020-09-27 15:50:00 +0200

    Contact form done

--- src/Contact.js
+++ src/Contact.js
@@ -1,17 +1,35 @@
 import React from 'react'
-import Layout from './components/Layout'
+import { Button, Form } from 'react-bootstrap';
+
 
 function Contact() {
     return (
-        <div className='content-wrap'>
-            <Layout>
-                <div>
-                    <h2>Contact Us</h2>
-                    <p>Text messaging, or texting, is the act of composing and sending electronic messages, typically consisting of alphabetic and numeric characters, between two or more users of mobile devices, desktops/laptops, or other type of compatible computer. Text messages may be sent over a cellular network, or may also be sent via an Internet connection. </p>
-                </div>
-            </Layout>
+        <div className='content-wrap, contact-page'>
+                <div className='contact-title'>Contact</div>
+                <div className='grey-line'></div>
+                <form id="contact-form" /*onSubmit={this.handleSubmit.bind(this)} method="POST"*/>
+                    <Form.Group>
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control type="text" placeholder="Enter name"></Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Subject</Form.Label>
+                        <Form.Control type="text" placeholder="Enter subject"></Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId="fromMessage">
+                        <Form.Label>Message</Form.Label>
+                        <Form.Control as="textarea" rows="5" placeholder='Write here...' />
+                    </Form.Group>
+                    <Button variant="dark" type="submit">
+                        Submit
+                </Button>
+                </form>
         </div>
-    )
+    );
 }
 
-export default Contact
+export default Contact;
