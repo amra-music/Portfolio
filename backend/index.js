debcb19fdcaf6bfde9326fe6772efa3a51acf656
Amra <amusic5@etf.unsa.ba>
2020-11-08 21:46:38 +0100

    Trailling slash fix

--- backend/index.js
+++ backend/index.js
@@ -59,5 +59,5 @@ router.post('/send', (req, res) => {
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('/', router);
+app.use('', router);
 app.listen(creds.PORT || 5000);
