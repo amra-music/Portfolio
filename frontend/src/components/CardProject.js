54167e3cb65b69b72cc18d7117a7439c2ac885f6
Amra <amusic5@etf.unsa.ba>
2020-11-17 23:03:56 +0100

    project cards flip effect done

--- frontend/src/components/CardProject.js
+++ frontend/src/components/CardProject.js
@@ -3,19 +3,20 @@ import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
 import battleship from '../assets/battleship.png';
 import augumentedReality from '../assets/augumentedReality.png';
+import web from '../assets/web.png';
 import payment from '../assets/payment.jpg';
 
 function cardProject() {
     return (
         <div className='cards-container'>
-            <Card className='card' style={{ width:500, height:285}}>
+            <Card className='card' style={{ width: 500, height: 285 }}>
                 <div className='card-inner'>
                     <div className='card-front'>
-                        <img alt='projectImage' src={battleship} style={{ width: '500px', height: '285px', borderRadius:20 }} />
+                        <img alt='projectImage' src={battleship} style={{ width: '500px', height: '285px', borderRadius: 20 }} />
                     </div>
-                    <div className='card-back'>
+                    <div className='card-back card-back-battleship'>
                         <Card.Body >
-                            <Link className='flip-card-link' to='/battleship'>Battleship</Link>
+                            <Link className='flip-card-link flip-card-link-battleship' to='/battleship'>Battleship</Link>
                             <Card.Text>
                                 Desktop strategy game Battleship implemented in the JavaFX framework with AI strategies comparison.
                     </Card.Text>
@@ -23,41 +24,50 @@ function cardProject() {
                     </div>
                 </div>
             </Card>
-            <Card className='card' border='info' style={{ width: '30rem', height: '25rem' }}>
-                <Card.Img variant="top" src={battleship} />
-                <Card.Body>
-                    <Link className='card-link' to='/battleship'>Battleship</Link>
-                    <Card.Text>
-                        Desktop strategy game Battleship implemented in the JavaFX framework with AI strategies comparison.
+            <Card className='card' style={{ width: 500, height: 285 }}>
+                <div className='card-inner'>
+                    <div className='card-front'>
+                        <img alt='projectImage' src={augumentedReality} style={{ width: '500px', height: '285px', borderRadius: 20 }} />
+                    </div>
+                    <div className='card-back'>
+                        <Card.Body >
+                            <Link className='flip-card-link' to='/augumentedReality'>Augumented reality Taj ul</Link>
+                            <Card.Text>
+                                Android application of a Taj Ul mosque designed in a Blender and imported into Unity.
                     </Card.Text>
-                </Card.Body>
+                        </Card.Body>
+                    </div>
+                </div>
             </Card>
-            <Card className='card' border='info' style={{ width: '30rem' }}>
-                <Card.Img variant="top" src={battleship} />
-                <Card.Body>
-                    <Link className='card-link' to='/webApp'>Web app</Link>
-                    <Card.Text>
-                        A web app developed for the Web technology subject at the Faculty of Electrical Engineering, Sarajevo
+            <Card className='card' style={{ width: 500, height: 285 }}>
+                <div className='card-inner'>
+                    <div className='card-front'>
+                        <img alt='projectImage' src={web} style={{ width: '500px', height: '285px', borderRadius: 20 }} />
+                    </div>
+                    <div className='card-back'>
+                        <Card.Body >
+                            <Link className='flip-card-link' to='/webApp'>Web app</Link>
+                            <Card.Text>
+                                A web app developed for the Web technology subject at the Faculty of Electrical Engineering, Sarajevo.
                     </Card.Text>
-                </Card.Body>
+                        </Card.Body>
+                    </div>
+                </div>
             </Card>
-            <Card className='card' border='info' style={{ width: '30rem', height: '25rem' }}>
-                <Card.Img variant="top" src={augumentedReality} />
-                <Card.Body>
-                    <Link className='card-link' to='/augumentedReality'>Augumented reality Taj ul</Link>
-                    <Card.Text>
-                        Android application of a Taj Ul mosque designed in a Blender and imported into Unity.
+            <Card className='card' style={{ width: 500, height: 285 }}>
+                <div className='card-inner'>
+                    <div className='card-front'>
+                        <img alt='projectImage' src={payment} style={{ width: '500px', height: '285px', borderRadius: 20 }} />
+                    </div>
+                    <div className='card-back'>
+                        <Card.Body >
+                            <Link className='flip-card-link' to='/paymentServer'>Payment server</Link>
+                            <Card.Text>
+                                Payment server is one part of information system for selling and charging products and services which is a big student project.
                     </Card.Text>
-                </Card.Body>
-            </Card>
-            <Card className='card' border='info' style={{ width: '30rem', height: '25rem' }}>
-                <Card.Img variant="top" src={payment} />
-                <Card.Body>
-                    <Link className='card-link' to='/paymentServer'>Payment server</Link>
-                    <Card.Text>
-                        Payment server is one part of information system for selling and charging products and services which is a big student project.
-                    </Card.Text>
-                </Card.Body>
+                        </Card.Body>
+                    </div>
+                </div>
             </Card>
         </div>
     )
