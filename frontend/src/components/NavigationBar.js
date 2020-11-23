699b2f5e8d1ec477aef07709497432ba63932bfe
Amra <amusic5@etf.unsa.ba>
2020-11-23 15:52:59 +0100

    <a> tag insade <a> tag in NavigationBar removed. NavigationBar updated

--- frontend/src/components/NavigationBar.js
+++ frontend/src/components/NavigationBar.js
@@ -1,45 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import { Nav, Navbar } from 'react-bootstrap';
-import styled from 'styled-components';
-
-const Styles = styled.div`
-  a, .navbar-brand, .navbar-nav .nav-link {
-    color: #bbb;
-
-    &:hover {
-      color: white;
-    }
-  }
-`;
 
 const NavigationBar = () => {
   return (
-    <Styles>
-      <Navbar expand="lg">
-        <Navbar.Brand href="/">Amra Musić</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Item>
-              <Nav.Link>
-                <Link className='navigation-link' to="/">Home</Link>
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link>
-                <Link className='navigation-link' to="/about">About</Link>
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link>
-                <Link className='navigation-link' to="/contact">Contact</Link>
-              </Nav.Link>
-            </Nav.Item>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </Styles >
+    <Navbar expand="lg">
+      <Link className='navigation-brand' href="/">Amra Musić</Link>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Item>
+            <Link className='navigation-link' to="/">Home</Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Link className='navigation-link' to="/about">About</Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Link className='navigation-link' to="/contact">Contact</Link>
+          </Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
